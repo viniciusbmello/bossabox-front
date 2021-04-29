934a3bc2af7451195d6d466609d1fea6d4869ee7
Vinícius Baptista de Mello <viniciusbmello@gmail.com>
2021-04-28 22:51:55 -0300

    List Tools

--- src/components/Tools/index.tsx
+++ src/components/Tools/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const Tool = ({ tool }) => {
-  const { id, title, link, description, tags } = tool;
+  const { title, link, description, tags } = tool;
   return (
     <div className="Card">
-      <h1 className="Card-title">
-        {id}. {title}
-      </h1>
-      <p className="Card-description">{description}</p>
-      <p className="Card-link">{link}</p>
-      <p className="Card-tags">{tags}</p>
+      <Link href={`${link}`}>
+        <h1 className="Card--title">{title}</h1>
+      </Link>
+      <p className="Card--description">{description}</p>
+      <span className="Card--tags">{tags.map(tag => `#${tag}`).join(' ')}</span>
     </div>
   );
 };
